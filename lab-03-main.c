8be1cf367c8e19828571bdf9988cccd834a4a4db
Yigit Isbiliroglu <ydeniz86@yahoo.com>
2013-05-07 14:16:21 -0400

    a small change

--- lab-03-main.c
+++ lab-03-main.c
@@ -7,32 +7,32 @@
 #include "lab-03.h"
 int main( int argc, char *argv[] )
 {
-        time_t startTime, stopTime;
-        clock_t startCPU, stopCPU;
-        double elapsedCPUsecs;
+	time_t startTime, stopTime;
+	clock_t startCPU, stopCPU;
+	double elapsedCPUsecs;
 
 
 
-        char **dictionary=malloc(50*sizeof(char*));
-        int count=0;
-        int wordcapacity=50;
-        if (argc < 3 )
-        {
-                printf("usage: ./%s <infileName> <outFilename>\n",argv[0]); /* you gotta put in & out file names on cmd line! */
-                return EXIT_FAILURE;
-        }       startTime = time( NULL );
-        printf("\nStarting load of %s at %s", argv[1], ctime( &startTime) );
+	char **dictionary=malloc(50*sizeof(char*));
+	int count=0;
+	int wordcapacity=50;
+	if (argc < 3 )
+	{
+		printf("usage: ./%s <infileName> <outFilename>\n",argv[0]); /* you gotta put in & out file names on cmd line! */
+		return EXIT_FAILURE;
+	}       startTime = time( NULL );
+	printf("\nStarting load of %s at %s", argv[1], ctime( &startTime) );
 
-        startCPU = clock();
-        loadDictionary( argv[1],&dictionary, &count, &wordcapacity);
-        stopCPU = clock();
-        stopTime = time( NULL );
-        printf("Finished load of %s at %s", argv[1], ctime( &stopTime) );
-        elapsedCPUsecs = ((double)(stopCPU-startCPU)) / CLOCKS_PER_SEC;
-        printf("Elapsed CPU seconds: %f\n",  elapsedCPUsecs );
-        doMenu( argv[2],  &dictionary, &count, &wordcapacity);
-        freeArray(dictionary, count);
-        return EXIT_SUCCESS; /* EXIT_SUCCESS defined as 0 in stdlib.h */
+	startCPU = clock();
+	loadDictionary( argv[1],&dictionary, &count, &wordcapacity);
+	stopCPU = clock();
+	stopTime = time( NULL );
+	printf("Finished load of %s at %s", argv[1], ctime( &stopTime) );
+	elapsedCPUsecs = ((double)(stopCPU-startCPU)) / CLOCKS_PER_SEC;
+	printf("Elapsed CPU seconds: %f\n",  elapsedCPUsecs );
+	doMenu( argv[2],  &dictionary, &count, &wordcapacity);
+	freeArray(dictionary, count);
+	return EXIT_SUCCESS; /* EXIT_SUCCESS defined as 0 in stdlib.h */
 }
 
 
